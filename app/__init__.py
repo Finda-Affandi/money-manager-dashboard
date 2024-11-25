57385377544c8e2859710e0622d18215c9a9172f
Finda Affandi <finda.affandi@sat.co.id>
2024-11-25 14:37:07 +0700

    adjust config

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from app.config import load_app_config
 from app.controller.main_controller import main_bp
 
 app = Flask(__name__)
-app.config.from_pyfile(os.path.join(os.path.dirname(__file__), 'config.py'))
+
+app_config = load_app_config()
+app.config.update(app_config)
 
 app.register_blueprint(main_bp)
--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
-APP_NAME = 'Money Manager Dashboard'
-FLASK_ENV = 'development'
-DEBUG = True
-SECRET_KEY = '45h80drweokd'
+import json
+import os
+
+
+def load_app_config():
+    app_config_path = os.path.join(os.path.dirname(__file__), 'config/app_config.json')
+    with open(app_config_path, 'r') as file:
+        app_config = json.load(file)
+
+    return app_config
