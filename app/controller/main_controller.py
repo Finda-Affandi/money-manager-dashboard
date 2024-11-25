4ff8cf042b2b12360ce1b7d6c06ce473caaaef29
Finda Affandi <finda.affandi@sat.co.id>
2024-11-25 14:25:14 +0700

    add index route and template

--- app/controller/main_controller.py
+++ app/controller/main_controller.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from operator import index
+
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@main_bp.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
