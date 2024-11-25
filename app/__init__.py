c2ded27ffb3866e670429a17a9a5c283239553bd
Finda Affandi <finda.affandi@sat.co.id>
2024-11-25 14:44:28 +0700

    add injector

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,12 @@ app = Flask(__name__)
 app_config = load_app_config()
 app.config.update(app_config)
 
+
+@app.context_processor
+def inject_config():
+    return {
+        'app_version': app_config.get('APP_VERSION'),
+    }
+
+
 app.register_blueprint(main_bp)
