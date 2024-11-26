e31751d1f8a74659b56f16a07c24ca45eaad070f
Finda Affandi <finda.affandi@sat.co.id>
2024-11-26 10:48:52 +0700

    add login_required decorators to route

--- app/controller/main_controller.py
+++ app/controller/main_controller.py
@@ -1,10 +1,11 @@
-from operator import index
-
 from flask import Blueprint, render_template
+
+from app.utils.decorators import login_required
 
 main_bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @main_bp.route('/', methods=['GET'])
+@login_required
 def index():
     return render_template('index.html')
