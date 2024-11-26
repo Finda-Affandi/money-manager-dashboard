48d8e0a50c647917f254882d329e1bead8dcd6e0
Finda Affandi <finda.affandi@sat.co.id>
2024-11-26 09:10:31 +0700

    adjust config system

--- app/config.py
+++ app/config.py
@@ -1,10 +1,19 @@
 import json
 import os
+from typing import Any, Tuple
 
 
-def load_app_config():
-    app_config_path = os.path.join(os.path.dirname(__file__), 'config/app_config.json')
-    with open(app_config_path, 'r') as file:
-        app_config = json.load(file)
+def load_all_config(config: str = None) -> tuple[Any, Any] | Any:
+    if config == 'app':
+        return load_config('app_config.json')
+    elif config == 'web':
+        return load_config('web_config.json')
+    else:
+        return load_config('app_config.json'), load_config('web_config.json')
 
-    return app_config
+
+def load_config(file: str):
+    config_path = os.path.join(os.path.dirname(__file__), 'config/' + file)
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    return config
