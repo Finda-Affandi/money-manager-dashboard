a8cb7d4bd1ea9a4433530b6dcb947ed76b696a8e
Finda Affandi <finda.affandi@sat.co.id>
2024-11-26 10:47:46 +0700

    adjust config variable name

--- app/config.py
+++ app/config.py
@@ -3,16 +3,16 @@ import os
 from typing import Any, Tuple
 
 
-def load_all_config(config: str = None) -> tuple[Any, Any] | Any:
+def load_config(config: str = None) -> tuple[Any, Any] | Any:
     if config == 'app':
-        return load_config('app_config.json')
+        return __load('app_config.json')
     elif config == 'web':
-        return load_config('web_config.json')
+        return __load('web_config.json')
     else:
-        return load_config('app_config.json'), load_config('web_config.json')
+        return __load('app_config.json'), __load('web_config.json')
 
 
-def load_config(file: str):
+def __load(file: str):
     config_path = os.path.join(os.path.dirname(__file__), 'config/' + file)
     with open(config_path, 'r') as file:
         config = json.load(file)
